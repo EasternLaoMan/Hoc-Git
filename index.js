788b5d0488fa35f3b64aff1342f846f592922fa6
Nguyen Tat Thang <thangnt.pasal@gmail.com>
2021-01-11 20:59:26 +0700

    csua code index.js & csua tittle index.html

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // Git commit -m "TenFileVuaChuyen" Enter nó sẽ chuyển File đó sang vùng nhớ 3
 // Khi File đang ở vùng nhớ 3, mà thao tác thêm, sửa, xóa ở File thì File bản sao sẽ được tạo ra, và nhảy về vùng nhớ 1, File cũ khi chưa sửa vẫn nằm ở vùng nhớ 3
 let sanpham = prompt("Nhập vào Tên sản phẩm");
-if(sanpham == "Gà") {console.log("Gia 10$");
-} else if(sanpham == "Bò") {
+if(sanpham === "Gà") {console.log("Gia 10$");
+} else if(sanpham === "Bò") {
     console.log("Gia 50$");
-} else if(sanpham = "Vịt") {console.log("Gia 15$");}
+} else if(sanpham === "Vịt") {console.log("Gia 15$");}
