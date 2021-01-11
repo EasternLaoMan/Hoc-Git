b4c7caecbdb8de6421a6df7171eb5aeb00bf0401
Nguyen Tat Thang <thangnt.pasal@gmail.com>
2021-01-11 20:29:24 +0700

    Them file app va sua index

---
+++ index.js
@@ -0,0 +1,3 @@
+// Git add TENFILEMUONCHUYENTUVUNGNHO1SANGVUNGNHO2 rồi Enter
+// Git commit -m "TenFileVuaChuyen" Enter nó sẽ chuyển File đó sang vùng nhớ 3
+// Khi File đang ở vùng nhớ 3, mà thao tác thêm, sửa, xóa ở File thì File bản sao sẽ được tạo ra, và nhảy về vùng nhớ 1, File cũ khi chưa sửa vẫn nằm ở vùng nhớ 3
