2e0d17c7d4285e15d73c7822af05ca79121e325e
Nguyen Tat Thang <thangnt.pasal@gmail.com>
2021-01-11 20:53:00 +0700

    chinh sua file index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 // Git add TENFILEMUONCHUYENTUVUNGNHO1SANGVUNGNHO2 rồi Enter
 // Git commit -m "TenFileVuaChuyen" Enter nó sẽ chuyển File đó sang vùng nhớ 3
 // Khi File đang ở vùng nhớ 3, mà thao tác thêm, sửa, xóa ở File thì File bản sao sẽ được tạo ra, và nhảy về vùng nhớ 1, File cũ khi chưa sửa vẫn nằm ở vùng nhớ 3
+let sanpham = prompt("Nhập vào Tên sản phẩm");
+if(sanpham == "Gà") {console.log("Gia 10$");} else if(sanpham == "Bò") {console.log("Gia 50$");}
